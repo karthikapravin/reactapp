53f679f1b1e422e66a7c4a21efbe10e6550ba261
Karthika <karthikamiruthvik@gmail.com>
2024-01-30 23:12:45 +0530

    fourth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload karthika kumar.
+          Edit <code>src/App.js</code> and save to reload karthika kumar renuga.
         </p>
         <a
           className="App-link"
