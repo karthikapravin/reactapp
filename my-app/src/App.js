d265e6f366a6148d1828d2e70ba7dc7dd78cfa7d
karthikapravin <158079349+karthikapravin@users.noreply.github.com>
2024-01-30 23:12:07 +0530

    fifth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload karthika kumar renuga.
+          Edit <code>src/App.js</code> and save to reload karthika kumar renuga suresh.
         </p>
         <a
           className="App-link"
